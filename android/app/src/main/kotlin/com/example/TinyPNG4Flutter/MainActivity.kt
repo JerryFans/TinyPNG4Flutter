902a4610f7996d00b1b8cbf1742deb914c8022fd
JerryFans <fanjiarong_haohao@163.com>
2021-12-20 21:02:11 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/TinyPNG4Flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.TinyPNG4Flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jerryfans/Library/Developer/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/jerryfans/OpenSource/TinyPNG4Flutter"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/jerryfans/OpenSource/TinyPNG4Flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
@@ -10,6 +10,6 @@ export "FLUTTER_FRAMEWORK_DIR=/Users/jerryfans/Library/Developer/flutter/bin/cac
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.packages"
