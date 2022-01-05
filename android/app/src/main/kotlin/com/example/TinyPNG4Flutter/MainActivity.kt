81495adf1b115e4291ea80cccdd4c6ad6897d366
JerryFans <fanjiarong_haohao@163.com>
2022-01-05 22:00:40 +0800

    1、add compress api 2、add file picker

--- android/app/src/main/kotlin/com/example/TinyPNG4Flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.TinyPNG4Flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,22 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'TinyPNG4Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -33,26 +34,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<File> fileList = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Future<Uint8List?> getImage() async {
+    var data = await rootBundle.load("images/test.jpg");
+    var buffer = data.buffer.asUint8List();
+    return await sendHttpProtocReq(buffer: buffer);
+  }
+
+  Future<Uint8List?> sendHttpProtocReq({@required Uint8List? buffer}) async {
+    var url = "api.tinify.com";
+    Uri uri = Uri.https(url, "/shrink");
+    var apiKey = "Y0v_xtfXJ6bSYkAITkCR6ROVKxw3BJK4";
+    var auth = "api:$apiKey";
+    var authData = base64Encode(utf8.encode(auth));
+    var authorizationHeader = "Basic " + authData;
+    var headers = {
+      "Accept": "application/json",
+      "Authorization": authorizationHeader
+    };
+    var response = await http.post(uri, headers: headers, body: buffer);
+    if (response.statusCode != 201) {
+      print("fail code is ${response.statusCode}");
+      return null;
+    } else {
+      var json = jsonDecode(utf8.decode(response.bodyBytes));
+      var jsonString = jsonEncode(json);
+      print("success json $jsonString");
+      return response.bodyBytes;
+    }
+  }
+
+  @override
+  void initState() {
+    getImage().then((value) {
+      setState(() {
+        // imageData = value;
+      });
     });
+    super.initState();
+  }
+
+  List<Widget> widgetList() {
+    var list = [
+            Text(
+              'Hello Word',
+            ),
+            TextButton(
+                onPressed: () async {
+                  FilePickerResult? result =
+                      await FilePicker.platform.pickFiles(allowMultiple: true);
+                  if (result != null) {
+                    List<File> files =
+                        result.paths.map((path) => File(path ?? "")).toList();
+                    List<File> chooseFiles = [];
+                    files.forEach((element) {
+                      if (element.path.toLowerCase().endsWith("jpg")
+                       || element.path.toLowerCase().endsWith("jpeg")
+                       || element.path.toLowerCase().endsWith("png")) {
+                         chooseFiles.add(element);
+                      } else {
+                        print("invalid image file : ${element.path}");
+                      }
+                    });
+                    if (chooseFiles.isNotEmpty) {
+                      setState(() {
+                      fileList.addAll(chooseFiles);
+                    });
+                    }
+                  } else {
+                    // User canceled the picker
+                  }
+                },
+                child: Text("添加"))
+          ];
+    fileList.forEach((element) { 
+      list.add(Image.file(element, height: 50, fit: BoxFit.fitHeight,));
+    });
+    return list;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-              'Hello Word',
-            ),
+      body: ListView(
+        children: widgetList(),
       ),
     );
   }
