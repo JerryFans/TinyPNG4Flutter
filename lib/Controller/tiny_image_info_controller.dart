f2235fbe7e2560b76a22d0ac93e045da7590001b
JerryFans <fanjiarong_haohao@163.com>
2022-01-06 22:11:20 +0800

    catch

--- lib/Controller/tiny_image_info_controller.dart
+++ lib/Controller/tiny_image_info_controller.dart
@@ -12,17 +12,16 @@ import 'package:path_provider_macos/path_provider_macos.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
 class TinyImageInfoController extends GetxController {
-  
   final PathProviderPlatform provider = PathProviderMacOS();
   var taskList = <TinyImageInfoItemViewModel>[].obs;
 
   void refreshWithFileList(List<File> files) {
     var vms = <TinyImageInfoItemViewModel>[];
-    files.forEach((element) { 
+    files.forEach((element) {
       vms.add(TinyImageInfoItemViewModel.file(element));
     });
-    vms.forEach((element) { 
-       beginCompressTask(vm: element);
+    vms.forEach((element) {
+      beginCompressTask(vm: element);
     });
     taskList.addAll(vms);
   }
@@ -30,7 +29,7 @@ class TinyImageInfoController extends GetxController {
   void beginCompressTask({required TinyImageInfoItemViewModel vm}) async {
     // var data = await rootBundle.load("images/test1.PNG");
     var data = await vm.file.readAsBytes();
-    
+
     var buffer = data.buffer.asUint8List();
 
     vm.status = TinyImageInfoStatus.uploading;
@@ -42,7 +41,7 @@ class TinyImageInfoController extends GetxController {
       vm.updateStatus(TinyImageInfoStatus.uploadFail);
       taskList.refresh();
       return;
-    } 
+    }
     vm.imageInfo = info;
     vm.updateStatus(TinyImageInfoStatus.downloading);
     taskList.refresh();
@@ -56,20 +55,24 @@ class TinyImageInfoController extends GetxController {
     }
 
     Directory? folder = await createDirectory(path, "tinyPngFlutterOutput");
-    
-    if (folder == null) { 
+
+    if (folder == null) {
       vm.updateStatus(TinyImageInfoStatus.downloadFail);
       taskList.refresh();
-      return; 
+      return;
     }
 
     vm.updateStatus(TinyImageInfoStatus.downloading);
     taskList.refresh();
     var compressFile = await createFile(folder.path, vm.file.fileName);
-    var isSuc = await downloadOutputImage(info,compressFile.path, onReceiveProgress: (count, total) {
-      vm.updateProgress(count, total);
-      taskList.refresh();
-    },);
+    var isSuc = await downloadOutputImage(
+      info,
+      compressFile.path,
+      onReceiveProgress: (count, total) {
+        vm.updateProgress(count, total);
+        taskList.refresh();
+      },
+    );
     if (isSuc) {
       vm.updateStatus(TinyImageInfoStatus.success);
       taskList.refresh();
@@ -90,19 +93,24 @@ class TinyImageInfoController extends GetxController {
       "Accept": "application/json",
       "Authorization": authorizationHeader
     };
-    var response = await http.post(uri, headers: headers, body: buffer);
-    if (response.statusCode != 201) {
-      print("fail code is ${response.statusCode}");
+    try {
+      var response = await http.post(uri, headers: headers, body: buffer);
+      if (response.statusCode != 201) {
+        print("fail code is ${response.statusCode}");
+        return null;
+      } else {
+        var json = jsonDecode(utf8.decode(response.bodyBytes));
+        var jsonString = jsonEncode(json);
+        print("success json $jsonString");
+        return TinyImageInfo.fromJson(json);
+      }
+    } catch (e) {
       return null;
-    } else {
-      var json = jsonDecode(utf8.decode(response.bodyBytes));
-      var jsonString = jsonEncode(json);
-      print("success json $jsonString");
-      return TinyImageInfo.fromJson(json);
     }
   }
 
-  Future<bool> downloadOutputImage(TinyImageInfo imageInfo, String savePath, {Function(int count, int total)? onReceiveProgress}) async {
+  Future<bool> downloadOutputImage(TinyImageInfo imageInfo, String savePath,
+      {Function(int count, int total)? onReceiveProgress}) async {
     String? url = imageInfo.output?.url;
     String? type = imageInfo.output?.type;
     if (url == null || type == null) {
@@ -110,25 +118,32 @@ class TinyImageInfoController extends GetxController {
     }
     Uri uri = Uri.parse(url);
     var dio = Dio();
-    var rsp = await dio.downloadUri(
-      uri, savePath, 
-      options: Options(headers: {"Accept": type, "Content-Type": "application/json"},),
-      onReceiveProgress: (count, total) {
-        onReceiveProgress?.call(count,total);
-      },
+    try {
+      var rsp = await dio.downloadUri(
+        uri,
+        savePath,
+        options: Options(
+          headers: {"Accept": type, "Content-Type": "application/json"},
+        ),
+        onReceiveProgress: (count, total) {
+          onReceiveProgress?.call(count, total);
+        },
       );
-    return rsp.statusCode == 200;
+      return rsp.statusCode == 200;
+    } catch (e) {
+      return false;
+    }
   }
 
-  Future<File> createFile(String path,String fileName) async {
+  Future<File> createFile(String path, String fileName) async {
     bool isExist = true;
-    var filePath = path+"/"+fileName;
+    var filePath = path + "/" + fileName;
     var count = 0;
     while (true) {
       if (count > 0) {
         var onlyName = fileName.split(".").first;
         var type = fileName.split(".").last;
-        filePath = path+"/"+onlyName+"_$count"+"."+type;
+        filePath = path + "/" + onlyName + "_$count" + "." + type;
       }
       isExist = await File(filePath).exists();
       print("try create path $filePath isExist $isExist");
@@ -140,8 +155,8 @@ class TinyImageInfoController extends GetxController {
     return await File(filePath).create();
   }
 
-  Future<Directory?> createDirectory(String path,String directoryName) async {
-    final filePath = path+"/"+directoryName;
+  Future<Directory?> createDirectory(String path, String directoryName) async {
+    final filePath = path + "/" + directoryName;
     var file = Directory(filePath);
     try {
       bool exist = await file.exists();
@@ -154,5 +169,4 @@ class TinyImageInfoController extends GetxController {
       return null;
     }
   }
-
 }
